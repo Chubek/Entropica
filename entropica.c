025af9ad322de6f3e642aeb422722ccce06ce140
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-05-08 02:30:06 +0330

    added defs destroy, revisions def

--- entropica.c
+++ entropica.c
@@ -9,25 +9,14 @@ entropica_semtable_t allocate_semtable() {
 	return allocate_shared_memory(MAX_CONCURRENT_READERS);
 }
 
-entropica_nonyieldt_t unallocate_semtable(entropica_semtable_t semtable) {
-	unallocate_shared_memory(semtable);
-}
-
 entropica_hashtable_t allocate_hashtable() {
 	return allocate_shared_memory(HASHTABLE_SIZE * sizeof(entropica_seqhash_t));
-}
-
-entropica_nonyieldt_t unallocate_hashtable(entropica_hashtable_t hashtable) {
-	unallocate_shared_memory(hashtable);
 }
 
 entropica_fofsttable_t allocate_fofsttable() {
 	return allocate_shared_memory(MAX_CONCURRENT_READERS * sizeof(entropica_fileofst_t));
 }
 
-entropica_nonyieldt_t unallocate_fofsttable(entropica_fofsttable_t fofsttable) {
-	unallocate_shared_memory(fofsttable);
-}
 
 entropica_readctx_t new_read_context(entropica_filepath_t filepath, entropica_procnum_t numprocesses) {
 	entropica_filedsc_t filedescriptor = open_file_path(filepath);
@@ -41,4 +30,12 @@ entropica_readctx_t new_read_context(entropica_filepath_t filepath, entropica_pr
 		.signals = signals,
 		.fileoffsets = fileoffsets,
 	};
+}
+
+
+entropica_nonyield_t destroy_read_context(entropica_readctx_t *context) {
+	close_file_descriptor(context->filedescriptor);
+	unallocate_shared_memory(context->entropyresults);
+	unallocate_shared_memory(context->signals);
+	unallocate_shared_memory(context->fileoffsets);
 }
